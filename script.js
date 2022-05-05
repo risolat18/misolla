09668cb5faf693b3298010a570fd93b9dcab7130
Risolat <risolat@gmail.com>
2022-05-05 14:25:08 +0500

    js 1 chi darsni yukladim

---
+++ script.js
@@ -0,0 +1,60 @@
+let ism = prompt('ism kiriting?')
+let ism1 = 'Mening ismim '+ism
+console.log(ism1);
+
+let yosh = prompt('yoshingizni kiriting?')
+let yosh1 ='Men '+yosh+' yoshdaman '
+console.log(yosh1);
+
+let misol1 =prompt('Misolni yeching 6+3=?')
+let togri1 = '9'
+let javob1 = 'Misol 1:'+' 6+3=9'+' Sizning javob '+misol1
+console.log(javob1);
+if (misol1 == togri1) {
+  alert("javobingiz to'g'ri")
+} else {
+  alert("javobingiz noto'g'ri")
+}
+
+
+let misol2 =prompt('Misolni yeching 20-5=?')
+let togri2 = '15'
+let javob2 = 'Misol 2:'+' 20-5=15 '+' Sizning javob '+misol2
+console.log(javob2);
+if (misol2 == togri2) {
+  alert("javobingiz to'g'ri")
+} else {
+  alert("javobingiz noto'g'ri")
+}
+
+
+let misol3 =prompt('Misolni yeching 15*2=?')
+let togri3 = '30'
+let javob3 = 'Misol 3:'+' 15*2=30 '+' Sizning javob '+misol3
+console.log(javob3);
+if (misol3 == togri3) {
+  alert("javobingiz to'g'ri")
+} else {
+  alert("javobingiz noto'g'ri")
+}
+
+let misol4 =prompt('Misolni yeching 8/4=?')
+let togri4 = '2'
+let javob4 = 'Misol 4:'+' 8/4=2 '+' Sizning javob '+misol4
+console.log(javob4);
+if (misol4 == togri4) {
+  alert("javobingiz to'g'ri")
+} else {
+  alert("javobingiz noto'g'ri")
+}
+
+let misol5 =prompt('Misolni yeching 10%3=?')
+let togri5 = '1'
+let javob5 = 'Misol 5:'+' 10%3=1 '+' Sizning javob '+misol5
+console.log(javob5);
+if (misol5 == togri5) {
+  alert("javobingiz to'g'ri")
+} else {
+  alert("javobingiz noto'g'ri")
+}
+
